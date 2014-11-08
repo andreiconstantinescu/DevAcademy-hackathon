19d6fade4d68bf98bb0f6b646f3b329ac5979c4b
Andrei Constantinescu <andrei.c@icloud.com>
2014-11-08 16:28:22 +0200

    Last commit.

--- DevApp.js
+++ DevApp.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
 	var userWins = 0;
 	var compWins = 0;
+	var currentMatch = 0;
 
 	function chewThem (userChoice) {
 		var compChoice = Math.floor(Math.random() * 5);
@@ -45,39 +46,28 @@ $(document).ready(function () {
 		var matchSet = choices[choice] + ' vs ' + choices[matchResult[1]] + ': Winner: ' + ((matchResult[0] !== 2) ? player[matchResult[0]] : 'Tie');
 		$('.small').text(matchSet);
 
-		//
-		// var history = localStorage.getItem('history');
-		// if (history !== null) {
-		// 	throw new Error().stack;
-		// 	var h = [];
-		// 	var has = localStorage.history;
-		// 	h.push(has);
-		// 	h.push(matchSet);
-		// 	localStorage.setItem('history', h);
-		//
-		// }
-		// else {
-		// 	localStorage.setItem('history', matchSet);console.log(matchSet);
-		// }
-		//
-
 		$('#history').prepend('<p>' + matchSet + '</p>');
 		if (matchResult[0] === 0) {
 			userWins++;
+			currentMatch++;
 		} else if (matchResult[0] === 1){
 			compWins++;
+			currentMatch++;
 		}
 
 		if (userWins === 2) {
 			$('#result').text('USER HAS WON THE GAME SET');
 			userWins = 0;
+			compWins = 0;
+			currentMatch = 0;
 		} else if (compWins === 2) {
 			$('#result').text('COMPUTER HAS WON THE GAME SET');
 			compWins = 0;
+			userWins = 0;
+			currentMatch = 0;
 		}
 
-		// console.log('userChoice', choice, choices[choice]);
-		// console.log('compChoice', matchResult[0], result);
+		// $('#status').html('<p>' + currentMatch + ' / 3');
 
 
 	});
